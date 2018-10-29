ca823e28408a5152cdea9168fc4516b0a078640e
yangyao <yao.yang@pactera.com>
2018-10-29 18:07:10 +0800

    测试Eureka和sys两个模块、流程

---
+++ itbo-service/itbo-service-sys/src/main/java/com/chizuko/itbo/service/sys/controller/HelloController.java
@@ -0,0 +1,37 @@
+package com.chizuko.itbo.service.sys.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author yangyao
+ * @Date 2018/10/23 0023 19:49
+ * @Version 1.0
+ **/
+
+@RestController
+public class HelloController {
+
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Autowired
+    private DiscoveryClient client;
+//
+    @GetMapping(value = "hello")
+    public String index() {
+
+        client.getServices().forEach(serviceId -> {
+            client.getInstances(serviceId).forEach(item -> logger.info("/hello, host:" + item.getHost() + ", service_id:" + item.getServiceId()));
+        });
+//        ServiceInstance instance = client.getLocalServiceInstance();
+//        logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+        return "Hello World";
+    }
+}
---
+++ itbo-service/itbo-service-sys/src/main/java/com/chizuko/itbo/service/sys/service/ItboSysApplication.java
@@ -0,0 +1,12 @@
+package com.chizuko.itbo.service.sys.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItboSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItboSysApplication.class, args);
+    }
+}
